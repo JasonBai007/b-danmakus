faa8c2e32c04575030c1b3aed04d5ff0c1c99485
JasonBai007 <byygkcg@163.com>
2021-04-23 14:41:23 +0800

    生成三种格式的文件

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 # 导入模块
 from bilibili_api import video
+import json
 
 # B站视频编号（示例）
 # 在每个视频URL里面（如下所示）：
@@ -18,18 +19,23 @@ page_id = info["pages"][0]["cid"]
 danmakus = video.get_danmaku(bvid=BVID, page_id=page_id)
 
 # 写入文件
-filename = 'result.txt'
+filename = 'res_all.txt'
 with open(filename, 'w', encoding='utf-8') as file_object:
     # 将List类型的数据转成字符串类型，转之前需要把里面的数值型的转成字符串型的
     file_object.write('\n'.join('%s' %id for id in danmakus))
 
-# 打印弹幕
+# 构建弹幕List
 danTextList = []
 for dm in danmakus:
     # print(str(dm))
     danTextList.append(dm.text)
 
+# 生成弹幕文本文件
 filename2 = 'res_text.txt'
 with open(filename2, 'w', encoding='utf-8') as file_object:
-    # 将List类型的数据转成字符串类型，转之前需要把里面的数值型的转成字符串型的
     file_object.write('\n'.join(danTextList))
+
+# 生成弹幕JSON文件
+filename3 = 'res_text.json'
+with open(filename3, 'w', encoding='utf-8') as file_object:
+    file_object.write(json.dumps(danTextList, indent=2, ensure_ascii=False))
